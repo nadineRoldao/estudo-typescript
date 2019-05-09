3eb8fc95274710b51fbb11ccfd6ba7cfb9a07711
Nadine Roldao <vitorianadineroldao@gmail.com>
2019-05-08 23:54:46 -0300

    exemplos adicionados

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-console.log('Hello TypeScript! ');
+export class Pessoa { }
+
+let a: string;
+let b: number;
+let c: Date;
+let p: Pessoa;
+let e: any;
+
+
+function somar(a: number, b: number = 10) {
+    console.log(a + b);    
+}
+
+function somar2(a: number, b?: number) {
+    if (!b) b = 10;    
+    console.log(a + b);    
+}
+
+function somar3(a: number, b: number): void {
+    console.log(a + b);    
+}
+
+function somar4(a: number, b: number): string {
+    // return a + b + '';
+    return '' + a + b;
+}
+
+console.log(somar4(10, 10));
